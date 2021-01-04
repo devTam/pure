c94a1522e44b13fa601f80db60df951087613a62
Teebreezy101 <tammy.batubo@gmail.com>
2021-01-04 16:10:58 +0100

    add highlights section

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,3 +50,26 @@ var x = setInterval(function() {
     document.getElementById("timer").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+// SWIPER JS
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
